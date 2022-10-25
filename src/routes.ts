1440ae10a35dc124e53ec515ee554890e30f284a
Henrique Barros <henriquebarrosx@gmail.com>
2022-10-24 22:57:13 -0300

    Create endpoints documentation

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express"
+import swaggerUi from "swagger-ui-express"
+
+import swaggerDocument from "./swagger.json"
 import { validateRequiredFields } from "@middlewares/create-transaction"
 import { listTransactionsController } from "@useCases/list-transactionts"
 import { createTransactionController } from "@useCases/create-transactions"
 
 export const routes = Router()
+
+routes.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 routes.get("/api/v1/transactions", (req, res) => {
     return listTransactionsController.handler(req, res)
